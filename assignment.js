39f7ae0e0c102190f1c3014d3974c41a3a305d2f
mahadihasan1555 <mahadihasan1555@gmail.com>
2020-08-06 00:08:03 +0600

    solved  task-1 feet to mile

---
+++ assignment.js
@@ -0,0 +1,19 @@
+// Problem-1 (convert from feet to mile)
+
+function feetToMile(feet){
+    var mile = feet / 5280;
+    return mile;
+}
+var carMile = feetToMile(100);
+console.log(carMile); 
+var bikeMile = feetToMile(1999);
+console.log(bikeMile); 
+var jetMile = feetToMile(1000000);
+console.log(jetMile); 
+
+// function feetToMile(feet){
+//     var 
+// }
+// Problem-2 (wood calculator)
+// Problem-3 (brick calculator)
+// Problem-4 (find smallest value)
