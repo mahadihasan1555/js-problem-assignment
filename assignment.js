478fcba580e11d767228ddef8493edf355f584ad
mahadihasan1555 <mahadihasan1555@gmail.com>
2020-08-07 00:13:28 +0600

    solved task-3 wood calculation

--- assignment.js
+++ assignment.js
@@ -13,9 +13,19 @@ console.log(jetMile);
 
 // Problem-2 (wood calculator)
 
+function woodCalculator(chair, table, bed){
+  var chairCount = chair * 1 ;
+  var tableCount = table * 3 ;
+  var bedCount = bed * 5 ;
+  var totalWood = chairCount + tableCount + bedCount;
+  return totalWood
+}
+var woodResult = woodCalculator(5, 7, 3);
+console.log(woodResult);
+
 // Problem-3 (brick calculator)
 
-// Problem-4 (find smallest array)
+// Problem-4 (find smallest string from array)
 
 var myArray = [ "mahadi", "mahadi hasan", "tamim khan", "sahid", "sahid hasan santo", "ashik", "nahid tonoy"];
 
